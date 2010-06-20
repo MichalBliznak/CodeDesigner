da4684998cf1f0267e4ca7b13ab2901f8eef3a6b
Michal Bližňák <MichalBliznak@users.noreply.github.com>
2010-06-20 22:43:52 +0000

    - Updated TODO.

--- src/UMLDesignerMain.cpp
+++ src/UMLDesignerMain.cpp
@@ -50,8 +50,6 @@ WX_DEFINE_OBJARRAY(DiagramsArray);
 
 WX_DEFINE_LIST(ListenerList);
 
-// TODO: allow linking of all (?) diagram component (like sub state)
-// TODO: fix instantiated classes in code items after class renaming
 // TODO: implement CodeLite plugin
 // TODO: implement languages by using XML templates/XSLT(???)
 // TODO: implement class instatiation by using STL containers(?)
