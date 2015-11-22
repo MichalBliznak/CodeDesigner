80c06e1becfdb3fbb44f47469ce8b5dbd0482e2b
michalbliznak <michalbliznak@ba5b78d9-efb8-4171-b4bf-c272399f8b04>
2015-11-22 10:51:06 +0000

    Changes in version 1.6.7 beta (2015-11-22): ------------------------------------------------ - Fixed build on 64-bit OS with MinGW 5.1

--- trunk/src/UMLDesignerMain.cpp
+++ trunk/src/UMLDesignerMain.cpp
@@ -1634,7 +1634,7 @@ void UMLDesignerFrame::OnAbout( wxCommandEvent &event )
 	svn = svn.SubString( 6, svn.Len() - 2 );
 	svn.Trim().Trim(false);
 	
-	wxString version = wxString::Format( wxT("1.6.6.%d Beta (SVN: %s) "), udvBUILD_NUMBER, svn.c_str() );
+	wxString version = wxString::Format( wxT("1.6.7.%d Beta (SVN: %s) "), udvBUILD_NUMBER, svn.c_str() );
 
     wxString desc = wxT("Cross-platform CASE tool designed for drawing of UML diagrams, code generation and reverse code engineering.\n\n");
 	desc << wxbuildinfo(long_f) << wxT("\n\n");
